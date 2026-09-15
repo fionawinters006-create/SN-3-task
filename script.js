741832bd86c841df9fb9bead38f9200e0c27c0ac
fionawinters006 <fionawinters006@gmail.com>
2026-09-15 22:32:45 +0500

    first commit

--- script.js
+++ script.js
@@ -3,3 +3,9 @@
 
 console.log("Hello, World!");
 console.log("This is a sample JavaScript file.");
+
+
+
+
+console.log("You can add more functionality to this script as needed.");
+console.log("Feel free to modify and expand the code to suit your requirements.");
